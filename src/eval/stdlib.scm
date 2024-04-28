5fc655259165b58aa748dcba57ff1ac2b95981af
Charles Eckman <charles@cceckman.com>
2024-04-28 11:56:31 -0400

    Implement set!
    Equality is a work-in-progress

--- src/eval/stdlib.scm
+++ src/eval/stdlib.scm
@@ -1,12 +1,22 @@
-(define foldl (
-  lambda (proc init x)
-  ; TODO: Use a let-expression here
-  (if (eq? (x) ())
-      (init)
-      (foldl (proc init (car x)) (cdr x))
-  )
-))
-(define + (
-  lambda (a . z)
-  (foldl sys:add a z)
-))
+; TODO: The general procedure alows multiple args,
+; but that would lead to mutual recursion with foldl.
+; Don't do that for now, until we have foldl working.
+; (define eq? (lambda (a b) (sys:eq? a b)))
+
+(define #t nil)
+(define #f nil)
+(set! #t (quote #t))
+(set! #f (quote #f))
+
+;; (define foldl (
+;;   lambda (proc init x)
+;;   ; TODO: Use a let-expression here
+;;   (if (eq? (x) ())
+;;       (init)
+;;       (foldl (proc init (car x)) (cdr x))
+;;   )
+;; ))
+;; (define + (
+;;   lambda (a . z)
+;;   (foldl sys:add a z)
+;; ))
---
+++ src/eval/stdlib_test.rs
@@ -0,0 +1,232 @@
+//! Tests for the standard library and bulitin functions.
+use super::*;
+use crate::data::{Object, Pair, Ptr};
+
+#[test]
+fn begin() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (define a (begin 1 2 3 4))
+        a
+        "#,
+    )
+    .unwrap();
+
+    match eval.eval().unwrap() {
+        Object::Integer(4) => (),
+        v => panic!("unexpected result: {v:?}"),
+    };
+}
+
+#[test]
+fn define_requires_symbol() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (define 1 39)
+        a
+        "#,
+    )
+    .unwrap();
+    match eval.eval() {
+        Err(Error::UserError(_)) => (),
+        v => panic!("unexpected eval result: {:?}", v),
+    };
+}
+
+#[test]
+fn define_requires_body() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (define a)
+        a
+        "#,
+    )
+    .unwrap();
+    match eval.eval() {
+        Err(Error::UserError(_)) => (),
+        v => panic!("unexpected eval result: {:?}", v),
+    };
+}
+
+#[test]
+fn define_body_is_expression() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (define a 1 2 3)
+        a
+        "#,
+    )
+    .unwrap();
+    match eval.eval() {
+        Err(Error::UserError(_)) => (),
+        v => panic!("unexpected eval result: {:?}", v),
+    };
+}
+
+#[test]
+fn nonempty_list() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (list 1 2 3)
+        "#,
+    )
+    .unwrap();
+    eval.eval().unwrap();
+    let mut head = eval.result_ptr().unwrap();
+    for value in [1, 2, 3] {
+        let pt: Ptr;
+        Pair { car: pt, cdr: head } = get_pair(eval.store(), head).unwrap();
+        match eval.store().get(pt) {
+            Object::Integer(v) => assert_eq!(v, value),
+            v => panic!(
+                "unexpected object: wanted integer {}, got {}",
+                value,
+                eval.store().display(v)
+            ),
+        }
+    }
+}
+
+#[test]
+fn empty_list() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (list)
+        "#,
+    )
+    .unwrap();
+    eval.eval().unwrap();
+    let head = eval.result_ptr().unwrap();
+    assert!(head.is_nil());
+}
+
+#[test]
+fn create_id_lambda() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (lambda (x) i)
+        "#,
+    )
+    .unwrap();
+
+    match eval.eval().unwrap() {
+        Object::Function(_) => (),
+        v => panic!("unexpected value: {v:?}"),
+    }
+}
+
+#[test]
+fn invoke_id() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (define id (lambda (x) x))
+        (id 7)
+        "#,
+    )
+    .unwrap();
+
+    match eval.eval().unwrap() {
+        Object::Integer(7) => (),
+        v => panic!("unexpected value: {v:?}"),
+    }
+}
+#[test]
+fn invoke_id_split() {
+    let mut eval = EvalEnvironment::new();
+    eval.start("(define id (lambda (x) x))").unwrap();
+    eval.eval().unwrap();
+    eval.start("(id 7)").unwrap();
+
+    match eval.eval().unwrap() {
+        Object::Integer(7) => (),
+        v => panic!("unexpected value: {v:?}"),
+    }
+}
+
+#[test]
+fn define_int_value_and_retrieve() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (define a 7)
+        (define b a)
+        b
+        "#,
+    )
+    .unwrap();
+
+    match eval.eval().unwrap() {
+        Object::Integer(7) => (),
+        v => panic!("unexpected result: {v:?}"),
+    };
+}
+
+#[test]
+fn sys_add_integers() {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+        (sys:add 5 3)
+        "#,
+    )
+    .unwrap();
+
+    match eval.eval().unwrap() {
+        Object::Integer(8) => (),
+        v => panic!("unexpected result: {v:?}"),
+    };
+}
+
+#[test]
+fn sys_add_floats() {
+    let mut eval = EvalEnvironment::new();
+    eval.start("(sys:add 0.5 1.5)").unwrap();
+
+    match eval.eval().unwrap() {
+        Object::Float(v) => {
+            // Yes, we're asserting equality rather than an approximation.
+            // 0.5, 1.5, and 2.0 all have power-of-two fractions and whole parts,
+            // so both should be exactly representations in any binary floating-point format.
+            // f64 especially.
+            assert_eq!(v, 2.0);
+        }
+        v => panic!("unexpected result: {v:?}"),
+    };
+}
+
+#[test]
+fn set() -> Result<(), Error> {
+    let mut eval = EvalEnvironment::new();
+    eval.start(
+        r#"
+    (define x 1)
+    (set! x 2)
+    x
+    "#,
+    )?
+    .eval()?;
+
+    match eval.result()? {
+        Object::Integer(2) => Ok(()),
+        x => panic!("unexpected result: {}", eval.store.display(x)),
+    }
+}
+
+// #[test]
+// fn booleans() -> Result<(), Error> {
+//     let mut eval = EvalEnvironment::new();
+//     eval.start("(eq? #t (quote #t))")?.eval()?;
+//     let t = eval.result_ptr()?;
+//     let hash_t = eval.store().put_symbol("#t");
+//     assert_eq!(t, hash_t);
+//
+//     Ok(())
+// }
