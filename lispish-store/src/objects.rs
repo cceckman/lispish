3982bcfbfcd856b867f4df116a2b181a90499bad
Charles Eckman <charles@cceckman.com>
2024-05-06 09:56:28 -0400

    Copy store into a new package, prep for vectors

---
+++ lispish-store/src/objects.rs
@@ -0,0 +1,266 @@
+use std::marker::PhantomData;
+
+use string_interner::{DefaultSymbol, Symbol as InternerSymbol};
+
+use super::{Bind, Storage, StoredPair, StoredPtr, StoredValue};
+
+/// Enum for a Lisp object.
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum Object<'a> {
+    Nil = StoredPtr::TAG_NIL,
+    Integer(Integer) = StoredPtr::TAG_INTEGER,
+    Float(Float) = StoredPtr::TAG_FLOAT,
+    Symbol(Symbol) = StoredPtr::TAG_SYMBOL,
+    Pair(Pair<'a>) = StoredPtr::TAG_PAIR,
+}
+
+/// An ID for a stored object: a combination of pointer and type-tag.
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Ptr<'a> {
+    pub(super) raw: StoredPtr,
+    store: PhantomData<&'a Storage>,
+}
+
+impl std::fmt::Display for Ptr<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.raw.fmt(f)
+    }
+}
+
+impl std::fmt::Display for StoredPtr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let tag = match self.tag() {
+            StoredPtr::TAG_NIL => "nil",
+            StoredPtr::TAG_INTEGER => "i64",
+            StoredPtr::TAG_FLOAT => "f64",
+            StoredPtr::TAG_SYMBOL => "sym",
+            StoredPtr::TAG_PAIR => "obj",
+            StoredPtr::TAG_BYTEVECTOR => "byt",
+            StoredPtr::TAG_VECTOR => "vec",
+            StoredPtr::TAG_STRING => "str",
+            _ => "???",
+        };
+        write!(f, "{}#{}", tag, self.idx())
+    }
+}
+
+impl std::str::FromStr for StoredPtr {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some((tag, number)) = s.split_once('#') {
+            let tag = match tag {
+                "nil" => StoredPtr::TAG_NIL,
+                "i64" => StoredPtr::TAG_INTEGER,
+                "f64" => StoredPtr::TAG_FLOAT,
+                "sym" => StoredPtr::TAG_SYMBOL,
+                "obj" => StoredPtr::TAG_PAIR,
+                "vec" => StoredPtr::TAG_VECTOR,
+                "byt" => StoredPtr::TAG_BYTEVECTOR,
+                "str" => StoredPtr::TAG_STRING,
+                _ => return Err(format!("invalid tag {}", tag)),
+            };
+            let idx: usize = number
+                .parse()
+                .map_err(|e| format!("invalid index: {}", e))?;
+            Ok(StoredPtr::new(idx, tag))
+        } else {
+            Err(format!("invalid pointer {}", s))
+        }
+    }
+}
+
+impl Ptr<'_> {
+    pub fn nil<'a>() -> Ptr<'a> {
+        Default::default()
+    }
+
+    #[inline]
+    pub fn is_nil(&self) -> bool {
+        self.raw.is_nil()
+    }
+    #[inline]
+    pub fn is_integer(&self) -> bool {
+        self.raw.is_integer()
+    }
+    #[inline]
+    pub fn is_float(&self) -> bool {
+        self.raw.is_float()
+    }
+    #[inline]
+    pub fn is_symbol(&self) -> bool {
+        self.raw.is_symbol()
+    }
+    #[inline]
+    pub fn is_pair(&self) -> bool {
+        self.raw.is_pair()
+    }
+
+    #[inline]
+    pub(super) fn tag(&self) -> u8 {
+        self.raw.tag()
+    }
+
+    #[inline]
+    pub(super) fn idx(&self) -> usize {
+        self.raw.idx()
+    }
+}
+
+impl Default for Ptr<'_> {
+    fn default() -> Self {
+        Ptr {
+            raw: StoredPtr { combined_tag: 0 },
+            store: PhantomData,
+        }
+    }
+}
+
+impl Object<'_> {
+    /// Create a nil object.
+    /// Nil objects are never stored, so this can be constructed directly (for convienient consing)
+    pub fn nil() -> Ptr<'static> {
+        Ptr::default()
+    }
+
+    pub(super) fn tag(&self) -> u8 {
+        // From https://doc.rust-lang.org/std/mem/fn.discriminant.html:
+        //
+        // SAFETY: Because `Self` is marked `repr(u8)`, its layout is a `repr(C)` `union`
+        // between `repr(C)` structs, each of which has the `u8` discriminant as its first
+        // field, so we can read the discriminant without offsetting the pointer.
+        unsafe { *<*const _>::from(self).cast::<u8>() }
+    }
+}
+
+impl From<i64> for Object<'_> {
+    fn from(value: i64) -> Self {
+        Object::Integer(value)
+    }
+}
+
+impl From<f64> for Object<'_> {
+    fn from(value: f64) -> Self {
+        Object::Float(value)
+    }
+}
+
+impl<'a> From<Pair<'a>> for Object<'a> {
+    fn from(value: Pair<'a>) -> Self {
+        Object::Pair(value)
+    }
+}
+
+impl From<Symbol> for Object<'_> {
+    fn from(value: Symbol) -> Self {
+        Object::Symbol(value)
+    }
+}
+
+impl<'a> TryInto<Pair<'a>> for Object<'a> {
+    type Error = &'static str;
+
+    fn try_into(self) -> Result<Pair<'a>, Self::Error> {
+        match self {
+            Object::Pair(p) => Ok(p),
+            _ => Err("object is not a pair"),
+        }
+    }
+}
+
+impl From<Object<'_>> for (StoredValue, u8) {
+    fn from(object: Object<'_>) -> Self {
+        match object {
+            Object::Nil => unreachable!("Do not serialize nil"),
+            Object::Symbol(_) => unreachable!("Symbols are interned, not stored"),
+            Object::Integer(i) => (StoredValue { integer: i }, object.tag()),
+            Object::Float(f) => (StoredValue { float: f }, object.tag()),
+            Object::Pair(p) => (
+                StoredValue {
+                    pair: StoredPair {
+                        car: p.car.raw,
+                        cdr: p.cdr.raw,
+                    },
+                },
+                object.tag(),
+            ),
+        }
+    }
+}
+
+impl<'a> Object<'a> {
+    pub(super) fn new(p: Ptr<'a>, v: StoredValue) -> Self {
+        let bind = |raw: StoredPair| -> Pair<'a> {
+            Pair {
+                car: Ptr {
+                    raw: raw.car,
+                    store: p.store,
+                },
+                cdr: Ptr {
+                    raw: raw.cdr,
+                    store: p.store,
+                },
+            }
+        };
+
+        match p.tag() {
+            StoredPtr::TAG_NIL => Object::Nil,
+            StoredPtr::TAG_INTEGER => Object::Integer(unsafe { v.integer }),
+            StoredPtr::TAG_FLOAT => Object::Float(unsafe { v.float }),
+            StoredPtr::TAG_SYMBOL => {
+                Object::Symbol(Symbol(DefaultSymbol::try_from_usize(p.idx()).unwrap()))
+            }
+            StoredPtr::TAG_PAIR => Object::Pair(bind(unsafe { v.pair })),
+            _ => panic!("invalid tag, possible data corruption"),
+        }
+    }
+}
+
+pub type Integer = i64;
+pub type Float = f64;
+
+impl std::fmt::Debug for Ptr<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Id")
+            .field("idx", &self.idx())
+            .field("tag", &self.tag())
+            .finish()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Pair<'a> {
+    pub car: Ptr<'a>,
+    pub cdr: Ptr<'a>,
+}
+
+impl<'a> Pair<'a> {
+    pub fn cons(car: Ptr<'a>, cdr: Ptr<'a>) -> Self {
+        Self { car, cdr }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Symbol(pub DefaultSymbol);
+
+impl<'a> Bind<'a> for Ptr<'a> {
+    type Free = StoredPtr;
+
+    fn bind(_store: &'a Storage, raw: Self::Free) -> Self {
+        Self {
+            raw,
+            store: PhantomData,
+        }
+    }
+}
+
+impl<'a> Bind<'a> for Pair<'a> {
+    type Free = StoredPair;
+
+    fn bind(store: &'a Storage, raw: Self::Free) -> Self {
+        Self {
+            car: Ptr::bind(store, raw.car),
+            cdr: Ptr::bind(store, raw.cdr),
+        }
+    }
+}
---
+++ lispish-store/src/render.rs
@@ -0,0 +1,244 @@
+use std::collections::{HashMap, VecDeque};
+
+use dot_writer::Attributes;
+use dot_writer::DotWriter;
+use std::io::Write;
+use std::process::Stdio;
+
+use crate::{Object, Pair, Ptr, Storage};
+
+use super::bitset::BitSet;
+use super::StorageStats;
+use super::StoredPtr;
+
+fn node_for_ptr(p: Ptr) -> String {
+    format!(r#"<{p}>"#)
+}
+
+/// Object metadata, for rendering purposes.
+#[derive(Default)]
+pub struct ObjectFormat {
+    pub label: String,
+    pub bg_color: String,
+}
+
+pub type ObjectFormats = HashMap<StoredPtr, ObjectFormat>;
+
+fn render_node<'a>(
+    store: &'a Storage,
+    object_meta: &ObjectFormats,
+    graph: &mut dot_writer::Scope,
+    it: Ptr<'a>,
+) -> (dot_writer::NodeId, Vec<Ptr<'a>>) {
+    let id = node_for_ptr(it);
+    let mut node = if it.is_symbol() {
+        graph.node_auto()
+    } else {
+        graph.node_named(&id)
+    };
+    let node_id = node.id();
+
+    let obj = store.get(it);
+    node.set_shape(dot_writer::Shape::None);
+
+    let get_name = |ptr: &Ptr| {
+        let name = object_meta
+            .get(&ptr.raw)
+            .map(|m| m.label.as_str())
+            .unwrap_or("");
+        if name.is_empty() {
+            maud::html!(
+                tr {
+                    td border="0" colspan="2" port="id" { (it) }
+                }
+            )
+        } else {
+            maud::html!(
+                tr { td border="0" colspan="2" port="id" { (it) }}
+                tr { td border="0" colspan="2" port="label" align="text" { b { (name) } }}
+            )
+        }
+    };
+    let name = get_name(&it);
+
+    fn single_value(v: impl std::fmt::Display) -> maud::PreEscaped<String> {
+        maud::html!(td colspan="2" { (v) })
+    }
+
+    let value: maud::PreEscaped<String> = match obj {
+        Object::Nil => unreachable!("must not queue to nil pointer"),
+        Object::Integer(v) => single_value(v),
+        Object::Float(v) => single_value(v),
+        Object::Symbol(v) => single_value(store.get_symbol(v)),
+        Object::Pair(Pair { car, cdr }) => maud::html!(
+                    td port="car" { (car) }
+                    td port="cdr" { (cdr) }
+        ),
+    };
+    node.set_html(&format!(
+        "<{}>",
+        maud::html!(
+            table {
+                (name)
+                tr { (value) }
+            }
+        )
+        .into_string()
+    ));
+
+    let car_port = node.id().port("car");
+    let cdr_port = node.id().port("cdr");
+    std::mem::drop(node);
+
+    // After completing the node, make the outbound edges.
+    let mut result = Vec::new();
+    if let Object::Pair(Pair { car, cdr }) = obj {
+        for (ptr, port) in [(car, car_port), (cdr, cdr_port)] {
+            if ptr.is_nil() {
+                continue;
+            }
+            // We always visit successors, even if we aren't going to render them,
+            // so we get an accurate count for stats.
+            result.push(ptr);
+
+            if ptr.is_symbol() {
+                // Symbols are addressed by their intern ID, not by an object.
+                // Generate a unique node for each visit.
+                // Unique them (so we don't have long edges to the definitions)
+                let (id, _) = render_node(store, object_meta, graph, ptr);
+                graph.edge(port, id);
+                result.push(ptr);
+            } else {
+                // Need to render the target node on the next pass.
+                let name = node_for_ptr(ptr);
+                graph.edge(port, name);
+            }
+        }
+    };
+    (node_id, result)
+}
+
+/// Render the state of storage into Graphviz graph.
+fn render_gv(store: &Storage, object_meta: &ObjectFormats) -> (StorageStats, Vec<u8>) {
+    let mut visited_objects = BitSet::new();
+    let mut visited_symbols = BitSet::new();
+    let mut stats = StorageStats::default();
+    let mut outbuf = Vec::new();
+    {
+        let mut writer = DotWriter::from(&mut outbuf);
+        let mut graph = writer.digraph();
+        graph.node_attributes().set_font("monospace");
+        let mut queue = VecDeque::new();
+        queue.push_back(store.root());
+
+        while let Some(it) = queue.pop_front() {
+            // Special-case symbols, since they have their own
+            // index space.
+            if it.is_symbol() {
+                if !visited_symbols.get(it.idx()) {
+                    stats.symbols += 1;
+                }
+                visited_symbols.set(it.idx());
+                continue;
+            }
+
+            if visited_objects.get(it.idx()) {
+                continue;
+            }
+            visited_objects.set(it.idx());
+            if it.is_nil() {
+                // We render nil only as a target; we don't insert a node for it.
+                continue;
+            }
+            stats.objects += 1;
+
+            let (_, next) = render_node(store, object_meta, &mut graph, it);
+            queue.extend(next);
+        }
+    }
+    (stats, outbuf)
+}
+
+impl StorageStats {
+    fn render(&self, header: &str) -> maud::PreEscaped<String> {
+        maud::html!(
+            strong { (header) ": " }
+            (self.objects) " objects / "
+            (self.string_data) " string bytes / "
+            (self.symbols) " symbols"
+        )
+    }
+}
+
+impl Storage {
+    pub fn render(&self) -> Result<maud::PreEscaped<String>, String> {
+        let labels = self.labels.borrow();
+        let (render_stats, gv) = render_gv(self, &labels);
+        std::mem::drop(labels);
+        let stats = self.current_stats();
+        let max_stats = self.max_stats();
+
+        // TODO: Work out how to make this async;
+        // we shouldn't really be holding on for it.
+        let graph_svg = move || -> Result<String, String> {
+            let mut dotgraph = std::process::Command::new("dot")
+                .arg("-Tsvg")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()
+                .map_err(|e| format!("failed to launch storage render: {e}"))?;
+            dotgraph
+                .stdin
+                .take()
+                .unwrap()
+                .write_all(&gv)
+                .map_err(|e| format!("failed to provide graphviz input: {e}"))?;
+            let dotgraph = dotgraph
+                .wait_with_output()
+                .map_err(|e| format!("failed to complete dot command: {e}"))?;
+
+            let save_graph = || {
+                let _ = tempfile::NamedTempFile::new()
+                    .and_then(|mut f| {
+                        f.write_all(&gv)?;
+                        let (_, pathbuf) = f.keep()?;
+                        Ok(pathbuf)
+                    })
+                    .map(|pathbuf| {
+                        tracing::info!("DOT source in {}", pathbuf.display());
+                    });
+            };
+            if !dotgraph.status.success() || std::env::var_os("LISPISH_SAVE_GRAPH").is_some() {
+                save_graph();
+            }
+
+            if dotgraph.status.success() {
+                Ok(String::from_utf8_lossy(&dotgraph.stdout).to_string())
+            } else {
+                Err(format!(
+                    "failed to render stack graph: dot failed: {}",
+                    &String::from_utf8_lossy(&dotgraph.stderr)
+                ))
+            }
+        }()
+        .map_err(|e| format!("error in rendering stack: {e}"))?;
+
+        Ok(maud::html!(
+                div class="heap" {
+                    h3 { "Storage" }
+                    p class="storage-stats" {
+                        (stats.render("current"))
+                        " | "
+                        (render_stats.render("traced"))
+                        " | "
+                        (max_stats.render("peak"))
+                    }
+                    div class="heap-display" {
+                        (maud::PreEscaped(graph_svg))
+                    }
+                }
+
+        ))
+    }
+}
